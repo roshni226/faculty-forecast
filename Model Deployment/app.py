dfb357238b4694d8b74fa592e8b295cf4de7317f
Shubhi <shubhi.paul2021@vitstudent.ac.in>
2023-06-03 18:53:46 +0530

    feat : vacancy table backend

--- Model Deployment/app.py
+++ Model Deployment/app.py
@@ -1,31 +1,17 @@
-import numpy as np
 from flask import Flask, request, jsonify,render_template
-import pickle
 import pandas as pd
 
-featdf=pd.read_csv('Model Deployment/features.csv')
-feat=[]
-for i in featdf.iterrows():
-    f=[]
-    for j in i:
-        f.append(j)
-    feat.append(f)
-
 app= Flask(__name__)
-model = pickle.load(open("Model Deployment\model.pkl","rb"))
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+data = pd.read_csv("Fdata.csv")
 
-@app.route("/predict",methods =['POST'])
-def predict():
-    #float_features = [float(x) for x in request.form.values()]
-    features= [np.array(feat[0])]
-    prediction = model.prediction(features)
+pivot_data = data.pivot_table(index=['EndYearPred', 'Designation'], columns='AreaOfSpecialization', aggfunc='size', fill_value=0)
 
-    return render_template("index.html",prediction_text = "Retirement years is {}".format(prediction))
+@app.route('/')
+def index():
+    return render_template('bootstrap_table.html', table=pivot_data.to_html(), )
 
-if __name__ =="__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True,port = 5500)
+
  
