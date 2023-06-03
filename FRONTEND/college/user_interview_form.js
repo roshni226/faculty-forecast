3589914c9f945ad6c7e0f3fee80b04ee781d1dc4
MohmikaK <mohmika.kapoor2021@vitstudent.ac.in>
2023-06-04 02:03:10 +0530

    updated college_table

---
+++ FRONTEND/college/user_interview_form.js
@@ -0,0 +1,18 @@
+class user_interview_form {
+    constructor(table, includeHeaders = true) {
+        this.table = table;
+        this.rows = Array.from(table.querySelectorall())
+    }
+
+    convertToCSV() {
+
+    }
+
+    _findLongestRowLength() {
+
+    }
+
+    static parseCell(tableCell) {
+
+    }
+}
