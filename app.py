4269150ce2e99a6ca74cacc58a1cf29050a731fa
Shubhi <shubhi.paul2021@vitstudent.ac.in>
2023-06-04 01:32:08 +0530

    feat: few minor tweaks

--- app.py
+++ app.py
@@ -11,7 +11,13 @@ data = pd.read_csv("final.csv")
 @app.route('/templates/college_table.html')
 def college():
     pivot_data = data.pivot_table(index=['RetirementYear', 'Designation'], columns='AreaOfSpecialization', aggfunc='size', fill_value=0)
-    return render_template('college_table.html', table=pivot_data.to_html(), )
+    return render_template('college_table.html', table=pivot_data.to_html())
+
+@app.route('/templates/vacancies_table.html')
+def vacancies():
+    grouped_data = data.groupby('RetirementYear').size().reset_index(name='total_vacancies')
+    grouped_data['Faculty Recruitment'] = grouped_data['total_vacancies'].apply(lambda x: 'TWO PHASE' if 30<x<60 else ('THREE PHASE' if 60<=x<180 else ('FOUR PHASE' if 180<=x else('USUAL'))))
+    return render_template('vacancies_table.html', table=grouped_data.to_html(index=False))
 
 if __name__ == '__main__':
     app.run(debug=True,port = 5500)
